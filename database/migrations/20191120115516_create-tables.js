adcc63aac9b1b727d5623d5f31709099c8b3946a
ofega <fegaeze@gmail.com>
2019-11-20 19:01:56 +0100

    Added db migration and seeds

--- database/migrations/20191120115516_create-tables.js
+++ database/migrations/20191120115516_create-tables.js
@@ -2,26 +2,26 @@
 exports.up = function(knex) {
     return knex.schema
         .createTable('users', table => {
-            table.increments();
+            table.increments('id');
             table.string('username', 255).notNullable().unique();
             table.string('email', 255).notNullable().unique();
             table.string('full_name', 255).notNullable();
             table.string('password', 50).notNullable()
         })
         .createTable('buckets', table => {
-            table.increments();
+            table.increments('id');
             table.string('bucket_name', 255).notNullable();
-            table.boolean('visibility').defaultTo(false);
+            table.boolean('visibility').defaultTo(0);
             table.date('deadline').notNullable();
-            table.integer('users_id')
+            table.integer('user_id')
                 .unsigned()
                 .notNullable()
                 .references('id').inTable('users')
                 .onDelete('CASCADE')
         })
         .createTable('todos', table => {
-            table.increments();
-            table.string('todos_name', 255).notNullable();
+            table.increments('id');
+            table.string('todo_name', 255).notNullable();
             table.boolean('completed').defaultTo(false);
             table.integer('bucket_id')
                 .unsigned()
@@ -29,10 +29,9 @@ exports.up = function(knex) {
                 .references('id').inTable('buckets')
                 .onDelete('CASCADE')
         })
-        .createTable('links', table => {
-            table.increments();
+        .createTable('activity_links', table => {
+            table.increments('id');
             table.string('url', 255).notNullable();
-            table.boolean('completed').defaultTo(false);
             table.integer('todo_id')
                 .unsigned()
                 .notNullable()
@@ -40,7 +39,7 @@ exports.up = function(knex) {
                 .onDelete('CASCADE')
         })
         .createTable('comments', table => {
-            table.increments();
+            table.increments('id');
             table.string('message', 255).notNullable();
             table.integer('bucket_id')
                 .unsigned()
---
+++ database/seeds/03-add-todos.js
@@ -0,0 +1,10 @@
+exports.seed = function(knex) {
+  return knex('todos').truncate()
+    .then(function () {
+      return knex('todos').insert([
+        {todo_name: 'Finish Lambda School', completed: 1, bucket_id: 3},
+        {todo_name: 'Travel to the moon', completed: 0, bucket_id: 2},
+        {todo_name: 'Seek adventure', bucket_id: 1}
+      ]);
+    });
+};
