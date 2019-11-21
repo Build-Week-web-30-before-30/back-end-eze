e62c8884ce2f5db5e4b738f174f541061882577b
ofega <fegaeze@gmail.com>
2019-11-21 15:02:04 +0100

    refactored seeds to app clean-up

---
+++ database/seeds/00-clean-up.js
@@ -0,0 +1,24 @@
+const cleaner = require('knex-cleaner');
+
+exports.seed = function(knex) {
+  return knex('users').truncate()
+    .then(() => {
+      return knex('buckets').truncate()
+        .then(() => {
+          return knex('todos').truncate()
+            .then(() => {
+              return knex('activity_links').truncate()
+                .then(() => {
+                    return knex('comments').truncate()
+                        .then(() => {
+                            return cleaner.clean(knex, {
+                                mode: 'truncate',
+                                restartIdentity: true, // Used to tell PostgresSQL to reset the ID counter
+                                ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
+                              });
+                        });
+                });
+            });
+        });
+    });
+};
