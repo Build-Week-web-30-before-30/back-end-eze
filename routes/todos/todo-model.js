f8b96dfb8b4496bff389ba2943cb5df02eb88708
ofega <fegaeze@gmail.com>
2019-11-21 20:24:36 +0100

    worked on todos models

---
+++ routes/todos/todo-model.js
@@ -0,0 +1,35 @@
+const db = require('../../database/dbConfig.js');
+
+function getAll() {
+    return db('todos');
+}
+
+function getByBucket(bucket_id) {
+    return db('todos').where({bucket_id: bucket_id});
+}
+
+function getTodoById(id) {
+    return db('todos').where({id: id});
+}
+
+async function add(todo) {
+    const [id] = await db('todos').insert(todo);
+
+    return getTodoById(id);
+}
+
+async function modify(todo_id, changes) {
+    await db('todos')
+        .update(changes)
+        .where({ id: todo_id })
+
+    return getTodoById(todo_id);
+}
+
+module.exports = {
+    add,
+    modify,
+    getByBucket,
+    getAll,
+    getTodoById
+};
